67d4dff110b2de7433a98c76979de6cf227de9d2
Swedeva <swethadevarajan570@gmail.com>
2023-05-01 09:32:51 +0000

    final commit

---
+++ dotnetapp/Context/yogaTrainerContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using dotnetapp.Models;
+//using  dotnetapp.AdminModel.Models;
+
+namespace dotnetapp.Context
+{
+    public class yogaTrainerContext : DbContext
+    {
+        public yogaTrainerContext(DbContextOptions<yogaTrainerContext> options) : base(options) 
+        { }
+        public DbSet<AdminModel> adminModels { get; set; }
+        public DbSet<UserModel> userModels { get; set; }
+        public DbSet<LoginModel> loginModels { get; set; }
+        public DbSet<JobModel> jobModels { get; set; }
+        public DbSet<jobSeekerModel> jobSeekers { get; set; }
+    }
+}
