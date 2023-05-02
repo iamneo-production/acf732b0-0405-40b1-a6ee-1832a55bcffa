0a7757fcc232d0216076cd0186a50223a940e7d1
Swedeva <swethadevarajan570@gmail.com>
2023-05-02 10:49:36 +0000

    final commit

---
+++ dotnetapp/Models/jobSeekerModel.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnetapp.Models
+{
+    public class jobSeekerModel
+    {
+        [Key]
+        public string personId { get; set; }
+        public string personName { get; set; }
+        public string personAddress { get; set; }
+        public string personExp { get; set; }
+        public string personPhone { get; set; }
+        public string PersonEmail { get; set; }
+        public int? JobId { get; set; }
+        [System.Text.Json.Serialization.JsonIgnore]
+        public JobModel? Job { get; set; }
+
+
+
+    }
+}
